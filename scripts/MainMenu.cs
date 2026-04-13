cba24e8bd54db494a085801ffa1ec6e5dcdd5ea5
ashniccole <89165773+ashniccole@users.noreply.github.com>
2026-04-13 13:08:02 -0700

    changed main menu / title screen
    main menu stuff

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -1,14 +1,16 @@
 using Godot;
+using System;
 
-public partial class TitleScreen : Control {
+public partial class MainMenu : Control
+{
 	Button startButton;
 	Button exitButton;
 
 	
 	public override void _Ready() {
 		GD.Print("Title screen ready.");
-		startButton = GetNode<Button>("Start");
-		exitButton = GetNode<Button>("Exit");
+		startButton = GetNode<Button>("VBoxContainer/Start");
+		exitButton = GetNode<Button>("VBoxContainer/Exit");
 
 		GD.Print("Start button: " + startButton);
 		GD.Print("Exit button: " + exitButton);

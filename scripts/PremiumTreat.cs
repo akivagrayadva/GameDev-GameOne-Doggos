3a56e4cc57f562f6c4d71da46beeaa13312de320
ashniccole <89165773+ashniccole@users.noreply.github.com>
2026-04-25 17:01:50 -0700

    premtreat added

--- scripts/PremiumTreat.cs
+++ scripts/PremiumTreat.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class PremiumTreat : Area2D
 {
+	private bool collected = false;
+
 	public override void _Ready()
 	{
 		BodyEntered += OnBodyEntered;
@@ -10,11 +12,15 @@ public partial class PremiumTreat : Area2D
 
 	private void OnBodyEntered(Node2D body)
 	{
+		if (collected) return;
+
 		if (body.Name == "DummyDog")
 		{
+			collected = true;
+
 			GD.Print("Premium treat collected!");
 
-			RoguelikeMovement.PremiumTreats += 1;
+			RoguelikeMovement.Instance.PremiumTreatCollected();
 
 			QueueFree();
 		}
